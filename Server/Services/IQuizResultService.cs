9b0c922869ad52ec0580cc21a77a8395976d1731
Vasyl Kovalov <raid3r81@gmail.com>
2023-05-21 19:35:59 +0300

    Create service interfaces

---
+++ Server/Services/IQuizResultService.cs
@@ -0,0 +1,14 @@
+﻿using CommonLibrary.LibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Services;
+
+internal interface IQuizResultService
+{
+    public ICollection<Quiz> GetAll();
+    public bool Add(QuizResult quizResult);
+}
---
+++ Server/Services/IQuizService.cs
@@ -0,0 +1,23 @@
+﻿using CommonLibrary.LibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Services;
+
+internal interface IQuizService
+{
+    public bool Add(Quiz quiz);
+    public bool Update(Quiz quiz);
+    public bool Delete(Quiz quiz);
+
+    public bool Add(Quiz quiz, Question question);
+    public bool Update(Question question);
+    public bool Delete(Question question);
+
+    public bool Add(Question question, Answer answer);
+    public bool Update(Answer answer);
+    public bool Delete(Answer answer);
+}
