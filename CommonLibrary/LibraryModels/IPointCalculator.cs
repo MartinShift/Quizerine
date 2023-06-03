238197765312e169e6cd1a8eb7b44fe6b4602abe
Vasyl Kovalov <raid3r81@gmail.com>
2023-06-03 21:06:02 +0300

    Create point calculator interface and realization version 1

---
+++ CommonLibrary/LibraryModels/IPointCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.LibraryModels
+{
+    public interface IPointCalculator
+    {
+        public int CalculatePoints(int totalTime, int spendTime, int questionCount, int correctQuesionCount);
+    }
+}
---
+++ CommonLibrary/LibraryModels/PointCalculatorVer01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.LibraryModels;
+
+public class PointCalculatorVer01 : IPointCalculator
+{
+    public int CalculatePoints(int totalTime, int spendTime, int questionCount, int correctQuesionCount)
+    {
+        const float BASE_FOR_CORRECT = 1000.0F;
+        const float BASE_FOR_TIME = 10.0F;
+
+        float persentOfCorrect = (float)correctQuesionCount / (float)questionCount;
+
+        float bonusForTime = ((float)totalTime - (float)spendTime) * BASE_FOR_TIME * persentOfCorrect;
+
+        return (int)((BASE_FOR_CORRECT * persentOfCorrect) + bonusForTime);
+    }
+}
